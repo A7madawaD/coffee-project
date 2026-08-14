a6682166da73146d7a8b29ae35bf76fd0a69e460
Ahmed Awad <ahmedawed878@gmail.com>
2026-08-14 18:46:40 +0300

    Update project

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+//start of hero
 const landingPage = document.querySelector(".head");
 
 const imgsArray = [
@@ -17,3 +18,30 @@ function changeBackground() {
 
 changeBackground();
 setInterval(changeBackground, 3000);
+// end of hero
+// start of nav
+const searchBtn = document.getElementById("searchBtn");
+const searchWrapper = document.getElementById("searchWrapper");
+const closeSearch = document.getElementById("closeSearch");
+const searchInput = document.getElementById("searchInput");
+
+searchBtn.addEventListener("click", function () {
+  searchWrapper.classList.add("active");
+
+  searchInput.focus();
+});
+
+closeSearch.addEventListener("click", function () {
+  searchWrapper.classList.remove("active");
+
+  searchInput.value = "";
+});
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape") {
+    searchWrapper.classList.remove("active");
+
+    searchInput.value = "";
+  }
+});
+// end of nav
